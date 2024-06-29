149daafe149161605586db75d125fdd103ca5cf1
Muxammadziyo000 <maximjonsen@gmail.com>
2024-06-29 15:56:17 +0500

    complated

--- src/companents/Introduction/Introduction.jsx
+++ src/companents/Introduction/Introduction.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './introduction.css'
 function Introduction() {
   return (
-    
+
     <div id='home' className='introduction'>
-    
+
       <div className="introduction_wrapper container">
         <p data-aos="fade-down" data-aos-duration="600" className='welcome_int'>Welcome to my portfolio</p>
         <div className="introduction_name">
@@ -12,14 +12,14 @@ function Introduction() {
           <p>  Muhammadziyo Nazarov</p>
 
         </div>
-       <div className="int_info_1">
-        <p data-aos="fade-down-right"  data-aos-duration="1500">Web developer ,</p>
-        <p data-aos="fade-down-left"  data-aos-duration="1500">Cool boy ,</p>
-       </div>
-       <div className="int_info_2">
-        <p data-aos="fade-up-right"  data-aos-duration="1500">Freeliancer ,</p>
-        <p data-aos="fade-up-left"  data-aos-duration="1500">Juniour  programmer,</p>
-       </div>
+        <div className="int_info_1">
+          <p data-aos="fade-down-right" data-aos-duration="1500">Web developer ,</p>
+          <p data-aos="fade-down-left" data-aos-duration="1500">Software engineer ,</p>
+        </div>
+        <div className="int_info_2">
+          <p data-aos="fade-up-right" data-aos-duration="1500">Freelancer ,</p>
+          <p data-aos="fade-up-left" data-aos-duration="1500">Front-end developer ,</p>
+        </div>
         <button><a href="#contact">CONTACT ME</a></button>
       </div>
     </div>
